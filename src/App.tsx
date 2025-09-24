c3589ff2d23673f917983554bddf1983ea61ca54
AgusJems <59831192+AgusJems@users.noreply.github.com>
2025-09-24 14:49:43 +0700

    aos

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,14 @@ import Testimonial from "./pages/Testimonial/Testimonial";
 import Poly from "./pages/Poly/Poly";
 import LandingLayout from "./layout/LandingLayout";
 import HomeLanding from "./pages/Home/Home";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+      AOS.init({ offset: 100, duration: 600, easing: "ease-in-sine", delay: 100 });
+    }, []);
   return (
     <>
       <Router>
