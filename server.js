973aff5a79c00756b044e6133635b1c1f6e776e1
AgusJems <59831192+AgusJems@users.noreply.github.com>
2025-12-05 16:25:38 +0700

    refactor: update server configuration to use environment variables and improve logging

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-// server.js
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger.js";
 import pool from "./config/db.js";
+
 import restoRoutes from "./src/server/routes/resto.routes.js";
 import newsRoutes from "./src/server/routes/news.routes.js";
 import poliRoutes from "./src/server/routes/poly.routes.js";
@@ -19,10 +22,9 @@ import carouselRoutes from "./src/server/routes/carousel.routes.js";
 import authRoutes from "./src/server/routes/auth.routes.js";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
@@ -35,7 +37,6 @@ app.use("/api", scheduleRoutes);
 app.use("/api", testimonialRoutes);
 app.use("/api", patnerRoutes);
 app.use("/api", inpatientRoutes);
-app.use("/api", inpatientRoutes);
 app.use("/api", emergencyRoutes);
 app.use("/api", supportRoutes);
 app.use("/api", carouselRoutes);
@@ -43,24 +44,19 @@ app.use("/api", restoRoutes);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Express backend listening at http://localhost:${port}`);
+// Start server
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Backend running at http://0.0.0.0:${port}`);
 });
 
-// Handle graceful shutdown and close the connection pool
+// Graceful shutdown
 process.on("SIGINT", async () => {
-  console.log("Received SIGINT. Closing database pool.");
   try {
-    await pool.end(); // Close the connection pool
-    console.log("MySQL database pool closed.");
+    await pool.end();
+    console.log("Database pool closed.");
     process.exit(0);
   } catch (err) {
-    console.error("Error closing database pool:", err);
+    console.error("Error closing DB:", err);
     process.exit(1);
   }
 });
-
-console.log("DB HOST:", process.env.DB_HOST);
-console.log("DB USER:", process.env.DB_USER);
-console.log("DB NAME:", process.env.DB_NAME);
